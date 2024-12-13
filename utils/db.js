8697b11b20238e4d138d33966ebe1f6d41f70255
tanimpersonal <tashintanim2019@gmail.com>
2024-12-13 08:12:38 +0600

    url-topology-removed

--- utils/db.js
+++ utils/db.js
@@ -2,11 +2,8 @@ const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
   try {
-    const uri = process.env.MONGO_URI
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const uri = process.env.MONGO_URI;
+    await mongoose.connect(uri);
     console.log("Connected to MongoDB successfully!");
   } catch (error) {
     console.error("Failed to connect to MongoDB:", error.message);
